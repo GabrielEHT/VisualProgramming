44d118e3ee5ac9955e994aed87e13eec80eae2e2
IllustredJelly <gabrielhernandez202333@gmail.com>
2022-03-09 17:02:08 -0400

    Improved node creation

---
+++ src/components/nodes.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    name:String,
+    type:String
+})
+</script>
+
+<template>
+  <div v-if="props.type=='assign'">
+    <h1>Assignation</h1>
+    <input placeholder="Variable name">
+  </div>
+  <div v-else>
+    <h3>{{props.name}}</h3>
+    <p v-if="props.type=='add'">A + B</p>
+    <p v-else-if="props.type=='sub'">A - B</p>
+    <p v-else-if="props.type=='mul'">A * B</p>
+    <p v-else-if="props.type=='div'">A / B</p>
+    <p v-else>Err</p>
+  </div>
+</template>
